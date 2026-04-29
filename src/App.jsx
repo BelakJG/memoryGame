4f817d6bc81322743107bcbaad6ff8fa2cc6161f
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-04-29 13:49:52 -0400

    new pokemon button now resets guesses

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ export default function App() {
       }
     }
     setMemoryValues(newPokemon);
+    setGuesses([]);
   }
 
   const [memoryValues, setMemoryValues] = useState([1,2,3,4,5,6,7,8,9,10,11,12]);
