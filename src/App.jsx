d8f1a58fd35441aa51676ce12c16c56fdf1d7696
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-04-29 11:35:09 -0400

    added buttons for game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
+import "./styles/app.css";
+
 export default function App() {
-  function shuffle([array]) {
+  function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       // Generate random index between 0 and i
       const j = Math.floor(Math.random() * (i + 1));
@@ -11,6 +13,9 @@ export default function App() {
     return array;
   }
   const memoryValues = [1,2,3,4,5,6,7,8,9,10,11,12];
+  const shuffledValues = [...memoryValues];
+  const memoryButtons = shuffledValues.map((number) => <button type="button">{number}</button>);
+
   const [guesses, setGuesses] = useState([]);
 
   return(<>
@@ -18,5 +23,8 @@ export default function App() {
       <h1>Memory Game</h1>
       <p>Correct guesses: {guesses.length}/{memoryValues.length}</p>
     </header>
+    <div id="game">
+      {memoryButtons}
+    </div>
   </>);
 }
